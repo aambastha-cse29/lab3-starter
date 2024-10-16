5d43e8c9d7edd82eeb03fda91329d6261bff34d3
Aman Ambastha <aambastha@ieng6-203.ucsd.edu>
2024-10-15 17:38:56 -0700

    FILE NAME CHANGE

---
+++ average2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * argc: The length of the argv array below
+ * argv: Array of strings: argv[0] holds the program name as a string; the remaining indices hold the actual command-line arguments
+ */
+int main(int argc, char *argv[]) {
+    int num_args = argc - 1; // argv[0] is program name
+    if (num_args < 2) {
+        printf("Error: Please provide at least two numbers.\nUsage: %s <number 1> <number 2> ...\n", argv[0]);
+        return 1;
+    }
+
+    float sum = 0;
+
+    // Loop through each command-line argument to calculate the sum
+    // First arg starts at argv[1]
+    int i;
+    for (i = 1; i < argc; i++) {
+        sum += atoi(argv[i]);
+    }
+
+    // Calculate the average of all args
+    float average = sum / (argc - 1);
+
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
